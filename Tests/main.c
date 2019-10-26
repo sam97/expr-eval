5d4565d1853274cdab4b542e598929eac1b5006a
Abdul Haseeb <abdulhaseeb@grabon.in>
2019-10-26 23:20:23 +0530

    Add CMocka-driven testing project

---
+++ Tests/main.c
@@ -0,0 +1,14 @@
+#include <cstdio>
+
+// Main function that is supposed to run all the tests at once.
+// Idea is to write test cases for each module in a separate file,
+// import it here and run the tests. CMocka will handle the rest.
+int main()
+{
+	printf("[+] Beginning tests...");
+
+
+
+	printf("[+] Tests complete.");
+	return 0;
+}
